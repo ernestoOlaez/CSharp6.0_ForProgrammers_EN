7a36d51480c6d00c5affbb49bf26188e96e914ac
DESKTOP-RP03BQR\Gisel <olaez_ernesto@hotmail.com>
2021-05-30 20:02:48 -0500

    Add Single class for Welcome to programming C# Languaje

---
+++ CSharp6.0_ForProgrammers/CSharp6.0_ForProgrammers/3.Introduccion/Welcome1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp6._0_ForProgrammers._3.Introduccion
+{
+    class Welcome1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Language Programming C#!");
+            Console.ReadLine();
+        }
+
+    }
+}
